febd044484ebab7971a1de7668d603ee0e798f9a
unknown <jhonerix23@gmail.com>
2024-05-28 21:24:29 +0800

    modified hero width

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,12 @@ import Button from './Button'
 function Hero() {
   return (
     <div className='flex flex-col md:flex-row items-center justify-center text-center h-screen px-20 gap-4'>
-        <div className="image">
+        <div className="image text-center">
             <img src={HomeMobile} alt="" className='md:hidden' />
             <img src={HomeDesktop} alt="" className='hidden md:block'/>
         </div>
-        <div className="description flex flex-col items-center md:items-start gap-4 md:w-1/2 md:text-left">
-            <h1 className='text-2xl md:text-6xl text-light-title-color font-semibold dark:text-dark-title-color'>Merry Christmas and Happy New Year!</h1>
+        <div className="description flex flex-col items-center md:items-start gap-4 md:w-[40%] md:text-left">
+            <h1 className='text-2xl md:text-5xl text-light-title-color font-semibold dark:text-dark-title-color'>Merry Christmas and Happy New Year!</h1>
             <p className='text-sm text-light-text-color dark:text-dark-text-color'>Christmas and a new year is about to begin, all good wishes and successes.</p>
             <Button btnText={'Get Started'}/> 
         </div>
