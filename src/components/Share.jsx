714ff3205e272e5197a66594df57d678f04da14a
unknown <jhonerix23@gmail.com>
2024-05-29 13:30:33 +0800

    modified share gifts

--- src/components/Share.jsx
+++ src/components/Share.jsx
@@ -6,10 +6,10 @@
     return (
         <div>
             <SectionTitle title={'Share A Gift'}/>
-            <div className='grid grid-cols-2 md:grid-cols-3 gap-4 py-10 px-4 md:px-40 lg:px-96'>
+            <div className='grid grid-cols-2 md:grid-cols-3 gap-4 py-10 px-4 md:px-24 lg:px-96'>
                 {
                     ShareGifts.map(share => (
-                        <div key={share.id} className=' dark:bg-dark-container-color py-4 rounded-lg shadow-md flex items-center flex-col place-items-center'>
+                        <div key={share.id} className='card hover:translate-y-[-5px] transition duration-100 dark:bg-dark-container-color py-4 rounded-lg shadow-md flex items-center flex-col place-items-center'>
                             {/* <div>
                                 <input id="heart" type="checkbox" />
                                 <label for="heart">❤</label>
