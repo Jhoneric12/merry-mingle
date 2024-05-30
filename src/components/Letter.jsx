03ad388e728f9e825ae91bc0a74410163c051584
unknown <jhonerix23@gmail.com>
2024-05-30 23:34:03 +0800

    modified send message button in letter

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -12,7 +12,7 @@ function Letter() {
                 <input type="text" className=' rounded-full py-4 px-6 w-[90%] md:w-full text-sm mt-6' placeholder='Write your message'/>
                 <Button
                     btnText={'Send Message'}
-                    styles={'bg-light-title-color w-[90%]'}
+                    styles={'bg-light-title-color w-[90%] md:w-[70%] lg:w-[60%]'}
                 />
             </div>
             <div className='flex justify-center items-center mt-6 z-10'>
